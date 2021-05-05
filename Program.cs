9d663ef6b4400d173c762c4e0da88923996c246c
Daniel <djbrittain2017@gmail.com>
2021-05-05 03:33:16 -0400

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RollingDie die = new RollingDie(10);
+            Warrior DJ = new Warrior("DJ", 100, 20, 10, die);
+            Warrior Victoria = new Warrior("Victoria", 60, 18, 15, die);
+            Arena arena = new Arena(DJ, Victoria, die);
+
+            arena.Fight();
+            Console.ReadKey();
+            
+        }
+
+
+    }
+}
---
+++ RollingDie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arena
+{
+    class RollingDie
+    {
+        private Random random; //creating private variables so they cant be accessed outside
+
+        private int sidesCount;
+
+        public RollingDie(int sidesCount)
+        {
+            this.sidesCount = sidesCount; // left variable belings to this instance and the right is treated as a parameter.
+            random = new Random();
+        }
+        public RollingDie() //default/ overloaded method if rollingdie int is not given
+        {
+            sidesCount = 6;
+            random = new Random();
+        }
+
+        public int GetSidesCount()
+        {
+            return sidesCount;
+        }
+
+        public int Roll()
+        {
+            return random.Next(1, sidesCount + 1);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Rolling a die with {0} sides", sidesCount);
+        }
+    }
+}
